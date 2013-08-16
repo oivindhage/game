5c150a8df5977ba0715361aabf364aaf233e56d8
Jo Are By <grimjoey@gmail.com>
2013-08-16 20:34:21 +0200

    removed unused parts of toys.js

--- helpers.js
+++ helpers.js
@@ -12,12 +12,19 @@ function tileYPos(y){
 
   
 function followCamera(obj,viewdata) {
-	xbuf = 96;
-	ybuf = 96;
-	xcam = gbox.getCamera().x;
-	ycam = gbox.getCamera().y;
-	if ((obj.x - xcam) > (gbox._screenw - xbuf)) gbox.setCameraX(xcam + (obj.x - xcam) - (gbox._screenw - xbuf), viewdata);
-	if ((obj.x - xcam) < (xbuf))                 gbox.setCameraX(xcam + (obj.x - xcam) - xbuf,                   viewdata);
-	if ((obj.y - ycam) > (gbox._screenh - ybuf)) gbox.setCameraY(ycam + (obj.y - ycam) - (gbox._screenh - ybuf), viewdata);
-	if ((obj.y - ycam) < (ybuf))                 gbox.setCameraY(ycam + (obj.y - ycam) - ybuf,                   viewdata);
+	var buf = {x:96, y:96};
+	var cam = gbox.getCamera();
+	
+	if ((obj.x - cam.x) > (gbox._screenw - buf.x)) gbox.setCameraX(cam.x + (obj.x - cam.x) - (gbox._screenw - buf.x), viewdata);
+	if ((obj.x - cam.x) < (buf.x))                 gbox.setCameraX(cam.x + (obj.x - cam.x) - buf.x,                   viewdata);
+	if ((obj.y - cam.y) > (gbox._screenh - buf.y)) gbox.setCameraY(cam.y + (obj.y - cam.y) - (gbox._screenh - buf.y), viewdata);
+	if ((obj.y - cam.y) < (buf.y))                 gbox.setCameraY(cam.y + (obj.y - cam.y) - buf.y,                   viewdata);
+    
+    try {
+        if (obj.fliph) {
+            //...
+        }
+    } catch(err) {
+        alert(err);
+    }
 }
